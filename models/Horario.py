ea2f8bc08508d8d0092225e73cb08ef6e260ef76
Eder Negrete <negretegarciaeib.cbtis13@gmail.com>
2022-12-13 00:58:52 -0600

    feat(models): se modifico el parametro nrc para que sea idClase

--- models/Horario.py
+++ models/Horario.py
@@ -7,16 +7,16 @@ class Horario_Model(db.Model):
     id = Column(Integer, primary_key= True)
     idSemana = Column(Integer, db.ForeignKey('diaSemana.id'))
     idSalon = Column(Integer, db.ForeignKey('salon.id'))
-    nrc = Column(String(20), db.ForeignKey('Clase.nrc'))
+    idClase = Column(String(20), db.ForeignKey('Clase.nrc'))
     horaInicio = Column(Time)
     horaFin = Column(Time)
     createdAt = Column(DateTime)
     updatedAt = Column(DateTime)
 
-    def __init__(self, idSemana, idSalon, nrc,horaInicio, horaFin, createdAt, updatedAt):
+    def __init__(self, idSemana, idSalon, idClase,horaInicio, horaFin, createdAt, updatedAt):
         self.idSemana = idSemana
         self.idSalon = idSalon
-        self.nrc = nrc
+        self.idClase = idClase
         self.horaInicio = horaInicio
         self.horaFin = horaFin
         self.createdAt = createdAt
